abad181a012fe0de3e9ab1e79f94a75ad1cdd64c
Ziad Mostafa <zeyadmostafa301@gmail.com>
2026-02-28 02:43:59 +0200

    fixing typo4

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 EXPECTED="Hello, Test!"
-OUTPUT=$(node -e "console.log(require('./app')('Test'));")
+OUTPUT=$(node -e "console.log(require('./app')('Test!'));")
 if [ "$OUTPUT" == "$EXPECTED" ]; then
   echo "Test passed!"
   exit 0
