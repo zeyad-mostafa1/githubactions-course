da86562950e718f6c57b35593a88bab80f5484d6
Ziad Mostafa <zeyadmostafa301@gmail.com>
2026-02-28 02:20:23 +0200

    Create a new NodeJs app

---
+++ src/app.js
@@ -0,0 +1,11 @@
+function great(name) {
+    return `Hello ${name}`;
+}
+
+module.exports = {
+    great
+}
+
+if (require.main === module) {
+    console.log(great('World'));
+}
---
+++ src/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+EXPECTED="Hello, World!"
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'));")
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+  echo "Test passed!"
+  exit 0
+else
+  echo "Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
+  exit 1
+fi
